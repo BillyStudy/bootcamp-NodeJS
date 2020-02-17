f8ad46e557a4614e77aef124f6e1496ec5a2e4c0
BillyStudy <billystudy@hotmail.com>
2020-02-17 19:24:22 -0300

    CRUD

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json());
+
 
 const users = ['Gustavo', 'Eder', 'Nathan'];
-server.get('/users/:index', (req, res) =>{
-    const { index } = req.params;
-    return res.json(users[index])
-});
+    
+    server.get('/users', (req, res) =>{
+        return res.json(users)
+    });
+
+    server.get('/users/:index', (req, res) =>{
+        const { index } = req.params;
+        return res.json(users[index])
+    });
+
+    server.post('/users/', (req, res) =>{
+        const { nome } = req.body;
+        users.push(nome);
+        return res.json(users);
+    });
+
+    server.put('/users/:index', (req, res) =>{
+        const { index } = req.params;
+        const { nome } = req.body;
+        users[index] = nome;
+        return res.json(users)
+    });
+
+    server.delete('/users/:index', (req, res) =>{
+        const { index } = req.params;
+        users.splice(index, 1);
+        return res.json(users)
+    });
 
 server.listen(3000);
 
